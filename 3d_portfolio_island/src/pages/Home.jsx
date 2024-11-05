612b9f587981611d884dc458978e845c7330314c
Daniel Chin <DanChin327@gmail.com>
2024-11-05 17:02:35 +0900

    added island component

--- 3d_portfolio_island/src/pages/Home.jsx
+++ 3d_portfolio_island/src/pages/Home.jsx
@@ -16,8 +16,11 @@ const Home = () => {
       >
 
       <Suspense fallback={<Loader />}>
-
-
+        <directionalLight />
+        <ambientLight />
+        <pointLight />
+        <spotLight />
+        <hemisphereLight />
       </Suspense>
 
 
