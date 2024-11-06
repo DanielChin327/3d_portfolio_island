c8452baa5cd9e6aac951e730118a3586da64aaeb
Daniel Chin <DanChin327@gmail.com>
2024-11-06 18:34:07 +0900

    refine past parts

--- 3d_portfolio_island/src/components/HomeInfo.jsx
+++ 3d_portfolio_island/src/components/HomeInfo.jsx
@@ -16,27 +16,27 @@ const InfoBox = ({text, link, btnText}) => (
 
 const renderContent = {
   1: (
-    <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5"> Hello, I am <span className="font-semibold">Daniel Chin</span>👋<br/>An aspiring software developer</h1>
+    <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5"> Hello there!👋 I am <span className="font-semibold">Daniel Chin</span>,<br/>An aspiring software developer! Welcome to my page! Feel free to take a look around!</h1>
   ),
   2: (
     <InfoBox
-      text="Worked with many companies and picked up many skills along the way."
+      text="Found out more about my skills and my journey!"
       link="/about"
-      btnText="Learn more"
+      btnText="About"
     />
   ),
   3: (
     <InfoBox
       text="Led many passionate projects. Please give them a look!"
       link="/projects"
-      btnText="My Portfolio"
+      btnText="Projects"
     />
   ),
   4: (
     <InfoBox
       text="You can contact me several ways! Feel free to reach out!"
       link="/contact"
-      btnText="Contact me"
+      btnText="Contact"
     />
   )
 }
