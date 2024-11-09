1733b69592873583bb87e75554ee7dedf8005b0f
Daniel Chin <danielchin@Daniels-MacBook-Pro.local>
2024-11-09 15:37:26 +0900

    changed routing to open on the about page first

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
-import { Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import Navbar from './components/Navbar';
-import {Home, About, Projects, Contact} from './pages'
+import { Home, About, Projects, Contact } from './pages';
 
 const App = () => {
+  const navigate = useNavigate();
+  const [firstLoad, setFirstLoad] = useState(true);
+
+  useEffect(() => {
+    if (firstLoad) {
+      navigate('/about'); // Redirect to About on the first load
+      setFirstLoad(false); // Set firstLoad to false after the initial redirect
+    }
+  }, [firstLoad, navigate]);
+
   return (
     <main className='bg-slate-300/20 h-full'>
-      <Router>
       <Navbar />
-        <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/about" element ={<About />}/>
-            <Route path="/projects" element ={<Projects />}/>
-            <Route path="/contact" element ={<Contact />}/>
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </main>
-  )
-}
+  );
+};
 
-export default App
+const RootApp = () => (
+  <Router>
+    <App />
+  </Router>
+);
+
+export default RootApp;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ import CTA from '../components/CTA'
 const About = () => {
   return (
     <section className="max-container">
-      <h1 className="head-text">Hi There! My name is <span className = "blue-gradient_text front-semibold drop-shadow">Dan</span>.
+      <h1 className="head-text">Hi There! My name is <span className = "blue-gradient_text front-semibold drop-shadow">Daniel Chin</span>.
       </h1>
 
       <div className = "mt-5 flex flex-col gap-3 text-slate-500">
